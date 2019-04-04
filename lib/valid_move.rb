1679842a24ba98262d56eb873ce625772311d030
Nicole Rothenay <nicoler@iccreativeagency.com>
2019-04-04 16:03:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,8 @@
 def valid_move?(board, index)
 if position_taken?(board, index) && board[index] != index.between?(0, 8)
   false
+elsif board[index] != index.between?(0, 8)
+  false
 else board[index] == !position_taken?(board, index) && index.between?(0, 8)
   true
 end
